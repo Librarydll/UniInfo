94bfa8e79a1bcba77f1e95a605bc4f05cdbbae26
Komil <library.dll@mail.ru>
2020-09-17 19:54:10 +0500

    s

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { Repository } from "./models/repository";
 import { Subject } from './models/subject.model';
-import { TranslateService } from '@ngx-translate/core';
 
 @Component({
     selector: 'app-root',
@@ -10,15 +9,7 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class AppComponent {
 
-  param = { value: 'world' };
-
-  constructor(translate: TranslateService) {
-    // this language will be used as a fallback when a translation isn't found in the current language
-    translate.setDefaultLang('en');
-
-    // the lang to use, if the lang isn't available, it will use the current loader to get them
-    translate.use('en');
-  }
+    constructor(private repo: Repository) { }
 
 
 }
--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -5,25 +5,11 @@ import { AppComponent } from './app.component';
 import { ModelModule } from "./models/model.module";
 import { StructureModule } from './structure/structure.module';
 import { FormsModule } from '@angular/forms';
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
-
-// AoT requires an exported function for factories
-export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http);
-}
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, ModelModule, StructureModule, HttpClientModule, TranslateModule.forRoot({
-    loader: {
-      provide: TranslateLoader,
-      useFactory: HttpLoaderFactory,
-      deps: [HttpClient]
-    }
-  })],
+  imports: [BrowserModule, AppRoutingModule ,FormsModule, ModelModule,StructureModule],
   providers: [],
   bootstrap: [AppComponent]
 })
